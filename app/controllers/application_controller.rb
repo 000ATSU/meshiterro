9df40ec5bc640a6ee221c6cb462bbc8d1899528c
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-05-11 01:38:16 +0000

    made show_view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    about_path
+    post_images_path  
   end
-  
+
   def after_sign_out_path_for(resource)
     about_path
   end
@@ -15,5 +15,5 @@ protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
 end
