81554b8a72b0deb8f01778465a9886f35985e34b
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-09-23 02:20:00 +0000

    [add]コメント機能

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,9 @@
 class PostImage < ApplicationRecord
 
-  has_one_attached :image
+  has_many :post_comments, dependent: :destroy
   belongs_to :user
+  
+  has_one_attached :image
 
   def get_image
     unless image.attached?
