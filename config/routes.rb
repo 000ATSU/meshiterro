eda69e4dff6c289bc2ca19c1bd905ae6136c6ed0
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-09-26 01:57:44 +0000

    [add]コメント削除機能、いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :post_comments, only: [:create]
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: 'about'
